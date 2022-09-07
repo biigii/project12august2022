bad25f89bbb55aafae8ce1bc43c7dd75db323155
behshad <82580731+biigii@users.noreply.github.com>
2022-09-07 21:02:50 +1200

    javascript

---
+++ scripts.js
@@ -0,0 +1,82 @@
+// camel casing
+
+// alert('hello world');
+// console.log("hello world"); // string
+// console.log('hello world');
+
+// console.log(1000) //number
+// console.log(2000)
+
+// console.log(100 + 200);
+// console.log(500-400);
+
+// console.log(200 + 200 - 50);
+// console.log("john" + " " + "smith");
+// console.log("1000");
+// console.log("1000" + "1000");
+// console.log("1000" + 1000);
+
+// variable
+// var age;
+// age = 90;
+// console.log(age);
+// age = 100;
+// console.log(age * age);
+// var age = 300;
+// console.log(age);
+
+// const age = 20;
+// console.log(age);
+// const age = 5;
+// age = 30;
+
+let age;
+age = 25;
+console.log(age);
+age = 40;
+console.log(age);
+
+const firstName = 'john';
+const lastName = 'smith';
+console.log(firstName + lastName);
+
+const num1 = 200;
+const num2 = 500;
+let total;
+
+console.log(total);
+total = num1 + num2;
+console.log(total);
+
+const text = 'hello world'; //string
+const num = 100;
+const isThisCorrect = true; //boolean
+// true or false   0 or 1  black or white on or off
+
+console.log(isThisCorrect);
+
+// array or list
+const numbers = [1, 2, 3];
+const fruits = ['apple', 'orange', 'kiwi'];
+// index 0 1 2
+// console.log(numbers);
+// console.log(fruits);
+console.log(fruits[3]);
+
+// javascript object 
+const employee1 = {
+  name: 'john',
+  surname: 'smith',
+  age: 20,
+  address: '123 street, city, country',
+  tel: 0211234567
+}
+
+console.log(employee1)
+console.log(employee1.address);
+console.log(employee1.age);
+
+// const mobileNumber = 021234567;
+// let address;
+console.log(document);
+
